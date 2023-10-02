ee75353694af0e6242334390b0d2126357487da7
MayraRobles <mlrobles@miners.utep.edu>
2023-10-02 07:55:31 -0600

    First progress

---
+++ src/tokenizer.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tokenizer.h>
+
+int space_char(char c)
+{
+  return ((c == '\t' || c == ' ') && c != '\0') ? 1 : 0;
+}
+
+int non_space_char(char c)
+{
+  return (c != '\t' && c != ' ' && c = '\0') ? 1 : 0; 
+}
+
+char *token_start(char *str)
+{
+  int c = 0, i = 0;
+  
+  while(space_char(c = *(str + i))) {
+    i++;
+  }
+  
+  return (c == '\0') ? NULL : str + i;
+}
+
+char *token_terminator(char *token)
+{
+  int i = 0;
+  
+  while(non_space_char(*(token + i))) {
+    i++;
+  }
+  
+  return token + i;
+}
+
+int count_tokens(char *str)
+{
+  int wordCount = 0;
+
+  char *tempPtr = token_start(str);
+
+  while(*tempPtr != NULL) {
+    if(non_space_char(*(tempPtr))) {
+      wordCount++;
+    }
+    tempPtr = token_terminator(tempPtr);
+    tempPtr = token_start(tempPtr);
+  }
+}
+
+char *copy_str(char *inStr, short len)
+{
+  char *copyStr = (char*)malloc((len + 1) * sizeof(char));
+
+  int i;
+  for(i = 0; i < len; i++) {
+    *(copyStr + i) = *(inStr + i);
+  }
+  *(copyStr + i) = '\0';
+  
+  return *copyStr;
+}
+
+char **tokenize(char  *str)
+{
+  int numTokens = count_tokens(str);
+  char **tokens = (char **)malloc((numTokens + 1) * sizeof(char *));
+  char *tempPtr = str;
+
+  int i;
+  for(i = 0; i < numTokens; i++) {
+    tempPtr = token_start(tempPtr);
+    int tokenLength = get_token_length(tempPtr); 
+    *(tokens + i) = copy_str(tempPtr, tokenLength);
+    tempPtr = token_terminator(tempPtr);
+  }
+  *(tokens + i) = 0;
+
+  return tokens;
+}
+
+void print_tokens(char **tokens)
+{
+  for(int i = 0; *(tokens + i) != 0; i++) {
+    printf("%s\n", *(tokens + i));
+  }
+}
+
+void free_tokens(char **tokens)
+{
+  for(int i = 0; *(tokens + i) != 0: i++) {
+    free(*(tokens + i));
+  }
+  free(tokens);
+}
+
+void get_token_length(*char tokenStart)
+{
+  int count = 0;
+  char c, *tempPtr = tokenStart;
+    
+  while (non_space_char(c = *(tempPtr + count))  && c != '\0') {
+    count++;
+  }
+
+  return count;
+}
+
+
+
---
+++ src/uimain.c
@@ -0,0 +1,19 @@
+// This is a file for the UI of the program
+#include <tokenizer.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  printf("---------------------------------------------------\n");
+  prinft("               WELCOME TO TOKENIZER!!\n");
+  printf("---------------------------------------------------\n\n");
+
+  while(1) {
+    printf("Greetings user!\n");
+    printf("Please enter the string you would like to tokenize:\n>\n");
+    printf("Would you like to continue? (y/n) ");
+    break;
+    
+  }
+  
+}
